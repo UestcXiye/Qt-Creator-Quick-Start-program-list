a6c0ecf48d5b1d1f305544bc77ab0c9d49e7ef0b
UestcXiye <58623498+UestcXiye@users.noreply.github.com>
2023-06-05 14:54:42 +0800

    Add files via upload

---
+++ 15/15-2/mydir/mainwindow.h
@@ -0,0 +1,28 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QFileSystemWatcher>
+#include <QMainWindow>
+
+namespace Ui
+{
+    class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget* parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow* ui;
+    QFileSystemWatcher myWatcher;
+
+private slots:
+    void showMessage(const QString& path);
+};
+
+#endif  // MAINWINDOW_H
---
+++ 15/15-2/mydir/mydir.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2023-06-05T14:38:25
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = mydir
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
